b979c63b2ffb151aca3727dc3ce93b595b32d598
TheeGreenGenie <solomonshasanmi@gmail.com>
2025-04-23 09:51:15 -0700

    venv and dependencies creation

---
+++ image_filtering.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+from tkinter import ttk, filedialog, Scale, HORIZONTAL
+from PIL import Image, ImageTk, ImageEnhance, ImageFilter, ImageOps
+import os
+import numpy as np
+from pathlib import Path
+import cv2
+
