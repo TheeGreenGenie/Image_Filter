972fbe58b65d0fe29285489da341d70d3e2cc15e
TheeGreenGenie <solomonshasanmi@gmail.com>
2025-04-23 10:11:28 -0700

    init and methods

--- image_filtering.py
+++ image_filtering.py
@@ -6,3 +6,128 @@ import numpy as np
 from pathlib import Path
 import cv2
 
+class ImageFilterApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Image Filtering App")
+        self.root.geometry("1200x800")
+
+        #variables
+        self.original_image = None
+        self.displayed_image = None
+        self.current_image = None
+        self.filename = None
+        self.export_format = tk.StringVar(value='png')
+        self.export_quality = tk.IntVar(value=90)
+        self.export_filename = tk.StringVar(value='filtered_image')
+
+        #track transformation states
+        self.rotation_angle = 0
+        self.flip_horizontal = False
+        self.flip_vertical = False
+        self.zoom_factor = 1.0
+
+        #Create UI
+        self.create_ui()
+
+        #setup drag & drop
+        self.setup_drag_drop()
+
+    def create_ui(self):
+        pass
+
+    def setup_drag_drop(self):
+        pass
+
+    def drop(self, event):
+        pass
+
+    def upload_image(self):
+        pass
+
+    def load_image(self, file_path):
+        pass
+
+    def display_image(self):
+        pass
+
+    def reset_to_original(self):
+        pass
+    
+    def apply_grayscale(self):
+        pass
+
+    def apply_sepia(self):
+        pass
+
+    def apply_negative(self):
+        pass
+
+    def apply_bw_threshold(self):
+        pass
+
+    def adjust_brightness(self, value):
+        pass
+
+    def adjust_contrast(self, value):
+        pass
+
+    def adjust_saturation(self, value):
+        pass
+
+    def adjust_blur(self, value):
+        pass
+
+    def adjust_sharpen(self, value):
+        pass
+
+    def rotate_image(self, degreese):
+        pass
+
+    def flip_horizontal_image(self):
+        pass
+
+    def flip_vertical_image(self):
+        pass
+
+    def zoom_in(self):
+        pass
+
+    def zoom_out(self):
+        pass
+
+    def reset_zoom(self):
+        pass
+
+    def start_crop(self):
+        pass
+
+    def on_crop_start(self):
+        pass
+
+    def on_crop_drag(self):
+        pass
+
+    def on_crop_release(self, event):
+        pass
+
+    def apply_edge_detection(self):
+        pass
+
+    def apply_vignette(self):
+        pass
+
+    def apply_vintage(self):
+        pass
+
+    def apply_color_inversion(self):
+        pass
+
+    def save_image(self):
+        pass
+
+    def save_before_after(self):
+        pass
+
+    def on_resize(self):
+        pass
